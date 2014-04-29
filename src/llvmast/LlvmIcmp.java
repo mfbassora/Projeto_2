ae4bf91715e349dcae7b839a673c6d5032bd7f38
gabrielpeixe <gpsarraff@gmail.com>
2014-04-29 19:47:12 -0300

    feito arrayassign, and, lessthan, equal

--- src/llvmast/LlvmIcmp.java
+++ src/llvmast/LlvmIcmp.java
@@ -1,11 +1,34 @@
 package llvmast;
 public  class LlvmIcmp extends LlvmInstruction{
+	public String cmp;
+	public LlvmRegister lhs;
+	public int conditionCode;
+	public LlvmType type;
+	public LlvmValue op1, op2;
     
     public LlvmIcmp(LlvmRegister lhs,  int conditionCode, LlvmType type, LlvmValue op1, LlvmValue op2){
+    	this.lhs = lhs;
+    	this.conditionCode = conditionCode;
+    	this.type = type;
+    	this.op1 = op1;
+    	this.op2 = op2;
+    	
+    	if(conditionCode == 1){
+    		cmp = "slt";
+    	}else if(conditionCode == 2){
+    		cmp = "and";
+    	}else{
+    		cmp = "eq";
+    	}
 
     }
 
     public String toString(){
-		return null;
+    	if(this.conditionCode == 2){
+    		return "and "+type+" "+op1+","+op2;
+    	}else{
+    		return "icmp "+cmp+" "+type+" "+op1+","+" "+op2;
+    		
+    	}
     }
 }
--- test/teste.java
+++ test/teste.java
@@ -12,7 +12,9 @@ class BC
 	public int soma()
 	{
 		int[] a;
-		a = new int[10];		
+		a = new int[10];	
+		a[1]=3;
+		
 		return 0;
 	}
 }
